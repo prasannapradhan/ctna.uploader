63a211ecea72309b74424783146e17926b3b6263
prasannapradhan <11991272+prasannapradhan@users.noreply.github.com>
2019-01-28 09:52:09 +0530

    fix oid

--- upload.php
+++ upload.php
@@ -6,3 +6,4 @@ error_log("Handling order [$oid]");
 error_reporting(E_ALL | E_STRICT);
 require('UploadHandler.php');
 $upload_handler = new UploadHandler();
+$upload_handler->options['oid'] = $oid;
