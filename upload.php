7aa02743b942ac4c7db9a6a6e2900c99b8048466
Prasanna Pradhan <pradhans.prasanna@gmail.com>
2019-10-12 16:41:13 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -4,7 +4,7 @@
 	$target_file = $target_dir. "/".$fname;
 	
 	$resp = array();
-	if(move_uploaded_file($_FILES['upic_uploader']['tmp_name'], $target_path)) {
+	if(move_uploaded_file($_FILES['upfile']['tmp_name'], $target_path)) {
 		$resp['status'] = 'SUCCESS';
 		$resp['fname'] = $fname;
 		echo json_encode($resp);
