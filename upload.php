97d840bff38e3eed309f62a0eadaee9cf882dc09
Prasanna Pradhan <pradhans.prasanna@gmail.com>
2019-10-12 17:51:18 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -4,7 +4,8 @@
 	$target_file = $target_dir. "/".$fname;
 	
 	error_log(json_encode($_FILES));
-
+	error_log(json_encode($_POST));
+	
 	$resp = array();
 	if(move_uploaded_file($_FILES['upfile']['tmp_name'], $target_file)) {
 		$resp['status'] = 'SUCCESS';
