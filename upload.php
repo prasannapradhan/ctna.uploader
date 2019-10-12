d35aa29e65ba6d2871b41c74ef431f182e8f200a
Prasanna Pradhan <pradhans.prasanna@gmail.com>
2019-10-12 18:23:03 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,10 +2,6 @@
 	$target_dir = "/var/www/html/resources/uploader";
 	$fname = "upic".time().".jpg";
 	$target_file = $target_dir. "/".$fname;
-	
-	$tmp_file = $_FILES['upfile']['tmp_name'];
-	error_log(file_get_contents($tmp_file));
-	
 	$resp = array();
 	if(move_uploaded_file($tmp_file, $target_file)) {
 		$resp['status'] = 'SUCCESS';
